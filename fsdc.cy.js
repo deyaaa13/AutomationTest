73ea30c9e80ff52a8e1efd3cc95aec83491f0129
Dhea Marenty Suwarno <dheamarentys@gmail.com>
2024-06-04 14:32:44 +0700

    Automation Test

--- fsdc.cy.js
+++ fsdc.cy.js
@@ -2,29 +2,35 @@ describe('DuniaCoding Website', () => {
     it('Should load the homepage', () => {
       cy.visit('http://duniacoding.id/');
       cy.url()
+      cy.get(':nth-child(1) > .nav-link');
       // Melakukan pengujian terhadap tampilan dan konten di halaman beranda
     });
     it('Should navigate to the "Courses" page', () => {
         cy.visit('http://duniacoding.id/'); // Ganti dengan URL halaman yang ingin diuji
-        cy.get(':nth-child(3) > .nav-link > span').click();
-// Tunggu beberapa saat untuk memastikan halaman selesai dimuat
+        cy.get(':nth-child(3) > .nav-link > span').click().should('be.visible');
+        cy.wait(2000);
+    });
+    it('Should navigate to the "Event" page', () => {
+        cy.visit('http://duniacoding.id/course'); // Ganti dengan URL halaman yang ingin diuji
+        cy.get(':nth-child(2) > .nav-link > span').click().should('be.visible');
         cy.wait(2000);
     });
     it('Login succesfully', () => {
         // Melakukan Login
-           cy.visit('http://duniacoding.id/course/');
-           cy.wait(2000);
+           cy.visit('http://duniacoding.id/event');
            cy.get('.col-12 > .btn').click();
+           cy.wait(2000);
            cy.url().should('include','/login');
     });
-    it('Should login without clearing the page', () => {
+    it('Should login with validate data', () => {
             // Kunjungi halaman login
             cy.visit('http://duniacoding.id/login/');
           
             // Masukkan email dan password ke dalam formulir login
             cy.get('#email').type('fitratuliman28@gmail.com');
             cy.get('#password').type('Bismillah2811');
-          
+            cy.get('#remember-me').click();
+            cy.wait(2000);
             // Submit formulir login menggunakan AJAX atau mekanisme lain yang tidak menyebabkan clear page
             cy.get('form').submit();
           
